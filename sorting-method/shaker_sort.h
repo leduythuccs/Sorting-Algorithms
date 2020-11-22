c20be8b6cfa6bea9a32362815eab9777eadd6c25
Le Duy Thuc <leduythuc.cs@gmail.com>
2020-11-22 13:40:18 +0700

    add optimize for shaker sort

--- sorting-method/shaker_sort.h
+++ sorting-method/shaker_sort.h
@@ -3,7 +3,6 @@
 
 #include "../helper/utils.h"
 void shaker_sort(int a[], int n) {
-    bool stop = true;
     int L = 0, R = n - 1;
     while (L < R) {
         for (int i = L; i < R; ++i)
@@ -21,13 +20,19 @@ void shaker_sort_optimize1(int a[], int n) {
     bool stop = true;
     int L = 0, R = n - 1;
     while (L < R) {
+        stop = true;
         for (int i = L; i < R; ++i)
             if (a[i] > a[i + 1])
-                swap(a[i], a[i + 1]);
+                swap(a[i], a[i + 1]), 
+                stop = false;
+        if (stop) break;
         --R;
+        stop = true;
         for (int i = R - 1; i >= L; --i)
             if (a[i] > a[i + 1])
-                swap(a[i], a[i + 1]);
+                swap(a[i], a[i + 1]),
+                stop = false;
+        if (stop) break;
         ++L;
     }
 }
